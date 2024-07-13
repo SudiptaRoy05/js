7b407a225dc42309a47b362b0aa8e3d3c41f8d03
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-07-14 00:22:24 +0600

    looping over array

--- array.js
+++ array.js
@@ -1,1 +1,18 @@
 let marks = [76, 56, 66, 97, 43];
+
+console.log(marks);
+console.log(marks.length);
+console.log(marks[0]);
+marks[2] = 75;
+console.log(marks)
+
+
+let heros = ["ironman", "hulk", "thor", "antman", "batman", "spiderman"];
+// console.log(heros);
+// console.log(heros.length);
+// console.log(typeof(heros));
+
+
+for(let i=0; i<heros.length; i++){
+    console.log(heros[i]);
+}
