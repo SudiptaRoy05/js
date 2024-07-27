fb0beb43415a1a1e0cb43f8514df37a4e1925843
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-07-27 23:49:24 +0600

    template literals

---
+++ strings.js
@@ -0,0 +1,16 @@
+let str = "amar sonar bangla";
+console.log(str.length);
+console.log(str[0]);
+
+let str1 = `cvbdszxdfcghbjnkm
+gchjknml,;uycvghytrx 7gtfvghbjn
+cvgbhnjkmuygvh uhygv ${str}`;
+
+console.log(str1)
+
+let obj ={
+    item : "pen",
+    price: 10
+};
+
+console.log(`the cost of ${obj.item} is ${obj.price}`)
