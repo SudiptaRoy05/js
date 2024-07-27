4ee37b3ee52336b77d371107cf23f30862fed59e
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-07-27 23:37:31 +0600

    game with loop

---
+++ game.js
@@ -0,0 +1,7 @@
+let gameNum = 15;
+let userNum = prompt("Guess the game number :");
+
+while(userNum != gameNum){
+    userNum = prompt("ypu entered wrong number try again :");
+}
+console.log("Congratulations u got this");
