8cb07ad1dfc20110576d2371d1169b94e3152d8d
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-07-31 10:48:47 +0600

    problem solved with foreach

---
+++ pSwithForeach.js
@@ -0,0 +1,13 @@
+let = arr = [2, 5, 3, 7, 9, 6];
+arr.forEach((value) => {
+    console.log(value*value);
+});
+
+
+
+let arr1 = [78,54,90,54];
+
+const calcSquare =(num)=>{
+    console.log(num*num);
+}
+arr1.forEach(calcSquare);
