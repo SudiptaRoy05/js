158560e9230b2e12c842b7913edda009d49c4f2e
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-08-09 12:10:59 +0600

    attribute in DOM

---
+++ DOM/attribute.js
@@ -0,0 +1,25 @@
+let div = document.querySelector("div");
+console.log(div)
+
+let id = div.getAttribute("id");
+console.log(id)
+
+
+div.style.backgroundColor = "pink"
+div.style.fontSize = "2rem"
+
+div.innerText = "Hola"
+// div.style.visibility =  "hidden"
+
+
+
+
+
+
+let name = div.getAttribute("name");
+console.log(name);
+
+
+let para = document.querySelector("p");
+console.log(para.setAttribute("class", "nC"));
+
