856d56872614aee79bc6dd0e796eeb219c1748a7
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-08-26 11:45:53 +0600

    append child

--- DOM/insert.js
+++ DOM/insert.js
@@ -11,6 +11,24 @@ div.after(newBtn);
 let newHeading = document.createElement("h1");
 newHeading.innerHTML = "<i>hi, I am new!</i>"
 document.querySelector("body").prepend(newHeading);
+document.querySelector("body").appendChild(newHeading) 
 
 let para = document.querySelector("p");
 para.remove();
+// newHeading.remove();
+
+// This doesn't append three paragraphs:
+// the three elements will be nested instead of siblings
+let p = document.body
+  .appendChild(document.createElement("section"))
+  .appendChild(document.createElement("div"))
+  .appendChild(document.createElement("p"));
+
+  p.innerText = "Hi i am p!";
+
+  let ids = document.createElement("div")
+
+  ids.innerHTML = "<h3>hii</h3>"
+  let sec = document.querySelector("section");
+  sec.append(ids);
+  
